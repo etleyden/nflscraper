bdad71cf471167a36a7447798623256746e41139
etleyden <etleyden@gmail.com>
2025-02-25 18:35:34 -0500

    Consolidated pipeline, started table API BUG FIXES: Weather bug (due to DB restructuring, scraping weather data broke).

--- src/dataviewer/nflscraper_backend/routes/table_routes.ts
+++ src/dataviewer/nflscraper_backend/routes/table_routes.ts
@@ -16,4 +16,19 @@ router.get("/tables", async (req, res) => {
     }
 });
 
+router.get("/table/:table_name", async (req, res) => {
+    try {
+        const query_string = `SELECT * FROM ${req.params.table_name}`;
+        console.log(query_string);
+        const result = await pool.query(`
+            SELECT *
+            FROM  ${req.params.table_name}
+        `);
+        res.send(`${result.rows.length} rows retrieved`);
+        console.log(result.rows);
+    } catch (error) {
+        res.status(500).json({message: (error as Error).message});
+    }
+});
+
 export default router;
